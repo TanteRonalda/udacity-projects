78c1ee94c2b7fba6e7a21720091604150c61cd4d
TanteRonalda <amikind08@gmail.com>
2021-05-09 12:15:21 +0200

    set a tags to be white and without decoration

--- project2_landing-page/js/app.js
+++ project2_landing-page/js/app.js
@@ -31,26 +31,23 @@
  * Begin Main Functions
  * 
 */
-document.addEventListener('DOMContentLoaded', dynamicNavigation);
+document.addEventListener("DOMContentLoaded", dynamicNavigation);
+document.addEventListener("DOMContentLoaded", addActiveClass);
+
 // build the nav
 function dynamicNavigation() {
-    const navList = document.getElementById("navbar__list");
     const section = document.querySelectorAll("section");
+    const navList = document.getElementById("navbar__list");
     for(let i = 0; i < section.length; i++) {
-        const navItem = document.createElement("li");
-        navItem.innerText = section[i].getAttribute("data-nav");
+        const navItem = document.createElement("li"); 
         const anchor = document.createElement("a");
+        anchor.innerText = section[i].getAttribute("data-nav");
+        anchor.setAttribute("id", "Anchor-Link" + "-" + section[i].id);
         anchor.setAttribute("href", "#"+section[i].id);
         navList.appendChild(navItem);
         navItem.appendChild(anchor);
     };
-}
-    
-
-
-    
-
-
+}  
 // Add class 'active' to section when near top of viewport
 
 
@@ -68,5 +65,18 @@ function dynamicNavigation() {
 // Scroll to section on link click
 
 // Set sections as active
+function addActiveClass () {
+    const navList = document.getElementById("navbar__list");
+    const navItems = document.querySelectorAll("li");
+        for(let i = 0; i < navItems.length; i++) {
+            navItems[i].addEventListener("click", function() {
+                const activeItem = document.getElementsByClassName("active");
+                    if (activeItem.length > 0){
+                        activeItem[0].className = activeItem[0].className.replace("active", "");
+                    }
+                    this.className += "active";
+            })
+        }
+}
 
 
