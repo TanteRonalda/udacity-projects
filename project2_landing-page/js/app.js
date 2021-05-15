3a09c8b95fc30b62f4efb182b28289b79a72c801
TanteRonalda <amikind08@gmail.com>
2021-05-15 22:33:50 +0200

    was accused of plagiarism by udacity, never seen the suspicios repo before, still reworked viewPortClass function

--- project2_landing-page/js/app.js
+++ project2_landing-page/js/app.js
@@ -20,44 +20,53 @@ function dynamicNavigation() {
 // Set navigation-items as active when they are clicked
 function addActiveClassClick () {
     const navItems = document.querySelectorAll("li");
-        for(let i = 0; i < navItems.length; i++) {
-            navItems[i].addEventListener("click", function() {
+    for(let i = 0; i < navItems.length; i++) {
+        navItems[i].addEventListener("click", function() {
             const activeItem = document.getElementsByClassName("active");
-                if (activeItem.length > 0){
-                    activeItem[0].className = activeItem[0].className.replace("active", "");
-                    }
-                    this.className = "active";
+            if (activeItem.length > 0)
+            {
+                activeItem[0].className = activeItem[0].className.replace("active", "");
+            } 
+            this.className = "active";
             });
         };
 }
 
-// Add class 'active' to section when near top of viewport
-function isInViewport(e) {
-    let rect = e.getBoundingClientRect();
-    return (
-       rect.top >= 0 
-   &&  rect.left >= 0
-   &&  rect.bottom <= (window.innerHeight || document.documentElement.clientHeight)
-   &&  rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-   );
-};
+/*const navItems = document.querySelectorAll("li");
 
-const contentSections = document.querySelectorAll("section");
-
-function viewportClass() {
-    for (viewedSection of contentSections) {
-        if  (isInViewport(viewedSection) === true) {
-            viewedSection.className = "in-view";
-
-        } else viewedSection.classList.remove("in-view");
-    };
+function ActivateNavItem(activeItem) {
+    for (navItem of navItems) {
+        if (activeItem.target === true )
+        {
+            navitem.className = "active";
+        }
+   }
 }
 
-document.addEventListener("scroll", function(){
-    viewportClass();
-});
+navItems.addEventListener("click", ActivateNavItem);
+*/
 
-const navItems = document.querySelectorAll("li");
+// Add class 'active' to section when near top of viewport
+function viewPortClass () {
+    const contentSections = document.querySelectorAll("section");
+    for (section of contentSections) {
+        const viewedSection = section.getBoundingClientRect();
+            if  (viewedSection.top >= 0 
+                &&  viewedSection.left >= 0
+                &&  viewedSection.bottom <= (window.innerHeight || document.documentElement.clientHeight)
+                &&  viewedSection.right <= (window.innerWidth || document.documentElement.clientWidth))
+            { 
+                section.classList.add("your-active-class");
+            } 
+            else section.classList.remove("your-active-class",);
+    
+    };
+};    
+
+
+document.addEventListener("scroll", function() {
+     viewPortClass();
+});
 
 // Scroll to section on link click  
 function scrollingSmoothly() {
@@ -70,8 +79,4 @@ function scrollingSmoothly() {
         const clickedAnchorLink = this.getAttribute("href");
         document.querySelector(clickedAnchorLink).scrollIntoView({behavior:"smooth"});
     }; 
-}
-
-
-
-
+};
