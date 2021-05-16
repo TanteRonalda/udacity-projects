946620a4bc6101051be939fafad0d5a5bc815f0d
TanteRonalda <amikind08@gmail.com>
2021-05-16 12:04:31 +0200

    added some ciation and tried to change addActiveClassClick

--- project2_landing-page/README.md
+++ project2_landing-page/README.md
@@ -19,4 +19,12 @@ I understand, that Udacity takes plagiarism very seriously and that copying anot
 
 When working on my project, I have looked at an amount of other peoples work - to get an idea how they were tackling the problems and also, because going through their code is helping me a lot with understanding, how different problems can be addressed and solved.
 
-I would like to point out though, that I have NEVER BEFORE looked at the repository  https://github.com/akristic/landing-page which is stated to be my main potential source in the plagiarism warning. 
+I would like to point out though, that I HAVE NOT copied code form any source,
+
+The repository  https://github.com/akristic/landing-page which is stated to be my main potential source in the plagiarism warning, I have NEVER opened before. Comparing the two now, I do see the similarities in the logic of some functions but I don't find it  justified to say that I have copied the entire scope. 
+
+As for the second potential source mentioned https://github.com/musaabhasan/fend2, I have also NEVER opened this one, however the user sounds familiar because because he posted in the Help Forum, as he was experiencing similar issues with his code to check whether a section is in viewport. I was having similar problems (which I also posted about in the Help Forum). I do understand, that I may have drawn a little too heavy on the code he posted with his question to fix mine, so that they ended up looking very much alike.
+
+-> That particular function I have now reworked and cited the source of the helper function (dimensions of viewport) in the code. That also wasn't an original from musaabhasan. 
+
+-> I have also included a lot more comments in the code, because I do understand it, which would also not be the case if I had just copy & pasted it.
--- project2_landing-page/js/app.js
+++ project2_landing-page/js/app.js
@@ -2,22 +2,23 @@ window.onload = dynamicNavigation;
 
 // Dynamically build the navigation menu
 function dynamicNavigation() {
-    const section = document.querySelectorAll("section");
+    const sections = document.querySelectorAll("section");
     const navList = document.getElementById("navbar__list");
-    for(let i = 0; i < section.length; i++) {
+    for(section of sections) {
         const navItem = document.createElement("li"); 
         const anchor = document.createElement("a");
-        anchor.innerText = section[i].getAttribute("data-nav");
-        anchor.setAttribute("id", "Anchor-Link" + "-" + section[i].id);
-        anchor.setAttribute("href", "#"+section[i].id);
+        anchor.innerText = section.getAttribute("data-nav");
+        anchor.setAttribute("id", section.id);
+        anchor.setAttribute("href", "#"+section.id);
         navList.appendChild(navItem);
         navItem.appendChild(anchor);
     };
     scrollingSmoothly();
-    addActiveClassClick();
 }  
 
+
 // Set navigation-items as active when they are clicked
+// How this is done ist very well explained here: https://www.w3schools.com/howto/howto_js_active_element.asp 
 function addActiveClassClick () {
     const navItems = document.querySelectorAll("li");
     for(let i = 0; i < navItems.length; i++) {
@@ -32,43 +33,91 @@ function addActiveClassClick () {
         };
 }
 
-/*const navItems = document.querySelectorAll("li");
 
-function ActivateNavItem(activeItem) {
+/*function addActiveClassClick () {
+    const navBar = document.querySelector("ul");
+        navBar.addEventListener("click", function ActivateNavItem(activeItem) {
+            const navItems = navBar.querySelectorAll("li");
+            for (navItem of navItems){
+                if (activeItem.target.nodeName === "LI") 
+                {   
+                navItem.ClassList.add("active");
+                }
+            }    
+            
+        });
+}
+
+/*function ActivateNavItem () {}
+    const anchorLinks = document.querySelectorAll("li a");
+    for (anchorLink of anchorLinks)
+        anchorLink.addEventlistener("click", function (activItem) {
+        if (activeItem.tagrget.nodeName === "A") 
+    {
+        anchorLink.className = "active"
+    }
+}
+
+(activeItem) {
     for (navItem of navItems) {
-        if (activeItem.target === true )
+        if (navItem.target === true )
         {
             navitem.className = "active";
         }
    }
 }
 
-navItems.addEventListener("click", ActivateNavItem);
+navBar.addEventListener("click", ActivateNavItem);
 */
 
 // Add class 'active' to section when near top of viewport
 function viewPortClass () {
-    const contentSections = document.querySelectorAll("section");
+    const contentSections = document.querySelectorAll(".landing__container");
     for (section of contentSections) {
         const viewedSection = section.getBoundingClientRect();
+        //To check the dimensions of viewedSection I have used a helper function, that can be found on multiple online sources, for example: https://www.javascripttutorial.net/dom/css/check-if-an-element-is-visible-in-the-viewport/
             if  (viewedSection.top >= 0 
                 &&  viewedSection.left >= 0
                 &&  viewedSection.bottom <= (window.innerHeight || document.documentElement.clientHeight)
-                &&  viewedSection.right <= (window.innerWidth || document.documentElement.clientWidth))
+                &&  viewedSection.right <= (window.innerWidth || document.documentElement.clientWidth)) 
             { 
                 section.classList.add("your-active-class");
             } 
             else section.classList.remove("your-active-class",);
-    
+            
     };
 };    
-
 
 document.addEventListener("scroll", function() {
      viewPortClass();
 });
 
-// Scroll to section on link click  
+/*//Highlight Item in Navigation when appropriate section is in Viewport
+function highlightNav () {
+    const contentSections = document.querySelectorAll("section");
+    const NavItem = document.querySelectorAll("li a")
+    for (section of contentSections) {
+        const viewedSection = section.getBoundingClientRect();
+        //To check the dimensions of viewedSection I have used a helper function, that can be found on multiple online sources, for example: https://www.javascripttutorial.net/dom/css/check-if-an-element-is-visible-in-the-viewport/
+            if  ((viewedSection.top >= 0 
+                &&  viewedSection.left >= 0
+                &&  viewedSection.bottom <= (window.innerHeight || document.documentElement.clientHeight)
+                &&  viewedSection.right <= (window.innerWidth || document.documentElement.clientWidth)) && (viewedSection.id == viewedNavItem.id))
+            { 
+                section.classList.add("your-active-class");
+                viewedNavItem.style.background = "red";
+            } 
+            else section.classList.remove("your-active-class",);
+            
+    };
+};    
+
+document.addEventListener("scroll", function() {
+     highlightNav();
+});*/
+
+// Scroll to section on link click
+// I would prefer to create this effect with CSS, but I believe it is required to be a JS function for this  project
 function scrollingSmoothly() {
     const anchorLinks = document.querySelectorAll("li a");
     for (const anchorLink of anchorLinks){
